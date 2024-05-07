40f79f2e564287cfecc7f7178550389dbaa48029
Lucas Gomes <lucasgomess2109@gmail.com>
2024-05-07 06:51:54 -0300

    feat: declarando uma variável

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 const myHeading = document.querySelector('h1');
 
 myHeading.textContent = 'Olá Mundo!';
+
+// Variáveis
+
+/*
+  Variables são contêineres que armazenam valores. Você começa declarando uma variável com a palavra-chave let, seguida do nome que você dá à variável: 
+ */
+
+let myVariable;
+// Depois de declarar uma variável, podemos atribuir um valor a ela:
+myVariable = 'Lucas';
