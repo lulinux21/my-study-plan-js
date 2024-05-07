c004265cbb5d51c1cb6b6bd5c4f4b9370abe61d5
Lucas Gomes <lucasgomess2109@gmail.com>
2024-05-07 06:54:30 -0300

    feat: add if/else basic

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,16 @@ myHeading.textContent = 'Olá Mundo!';
 let myVariable;
 // Depois de declarar uma variável, podemos atribuir um valor a ela:
 myVariable = 'Lucas';
+
+// Condicionais
+/*
+  Condicionais são estruturas de código usadas para testar se uma expressão retorna verdadeira ou não. Uma forma muito comum de condicionais é a instrução if...else. Por exemplo:
+*/
+
+let iceCream = 'chocolate';
+
+if (iceCream === 'chocolate') {
+  alert('Sim, eu amo sorvete de chocolate!');
+} else {
+  alert('Aaaah, mas chocolate é o meu favorito...');
+}
