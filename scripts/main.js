62822e5b178c8e13545ee1065c56d6a5cb592c8f
Lucas Gomes <lucasgomess2109@gmail.com>
2024-05-07 08:25:20 -0300

    feat: finaliza o tutorial de js básico

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,28 @@ myImage.onclick = () => {
     myImage.setAttribute('src', 'images/firefox-icon.png');
   }
 };
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('Por favor, digite o seu nome');
+
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Seja bem-vindo, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `Bem-vindo novamente, ${storedName}`;
+}
+
+myButton.addEventListener('click', () => {
+  setUserName();
+});
